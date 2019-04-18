7a9b5ac73e9520f2777a02d8108cc4d78bd3968e
Bana <banabatshon@hotmail.com>
2019-04-18 18:19:51 +0000

    modifies the index, new and create methods allowing the admin to list and create new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,12 +1,20 @@
 class Admin::CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Product.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 end
