a3cb54db093fdf67b471530cd96e2afbc248261f
Bana <banabatshon@hotmail.com>
2019-04-18 21:43:49 +0000

    defines the new, create and destroy methods for logging in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # convert email to lowercase to match email in db incase the user has capslock on
+    user = User.find_by(email: params[:login][:email].downcase)
+    # verify user exists in db and run the authenticate method to check if submitted password in form is correct
+    if user && user.authenticate(params[:login][:password])
+      # save the user in the cookie session
+      session[:user_id] = user.id.to_s
+      redirect_to root_path, notice: "You are logged in!"
+    else
+      flash.now.alert = "Incorrect email or password, try again."
+      render :new
+    end
+  end
+
+  def destroy
+    # delete the user_id from the cookie session
+    session.delete(:user_id)
+    redirect_to login_path, notice: "You are logged out!"
+  end
 end
