ddd8f3d586cdd86553feb57ae7ffe3e0662a1576
Bana <banabatshon@hotmail.com>
2019-04-19 23:31:51 +0000

    removes the default vlaues for the timestamp

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,7 @@
 class Review < ActiveRecord::Base
-  before_save :default_values
   belongs_to :product
   belongs_to :user
   validates :product_id, presence: true
   validates :description, presence: true
   validates :rating, presence: true
-  def default_values
-    self.createdat ||= "default value"
-    self.updatedat ||= "default value"
-  end
 end
