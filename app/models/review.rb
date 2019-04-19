e5316090538c3cf55cabf99cd110484bc6e0865b
Bana <banabatshon@hotmail.com>
2019-04-19 22:20:37 +0000

    adds validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,10 @@
 class Review < ActiveRecord::Base
   before_save :default_values
+  belongs_to :product
+  belongs_to :user
+  validates :product_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
   def default_values
     self.createdat ||= "default value"
     self.updatedat ||= "default value"
