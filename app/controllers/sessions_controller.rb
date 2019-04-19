eb79fa44fdbad7ecb0dd6b856edacb80769a0a86
Bana <banabatshon@hotmail.com>
2019-04-19 02:16:50 +0000

    changes the way the method find_user_by accesses the params, was missing login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     # convert email to lowercase to match email in db incase the user has capslock on
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:login][:email])
     # verify user exists in db and run the authenticate method to check if submitted password in form is correct
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:login][:password])
       # save the user in the cookie session
       session[:user_id] = user.id.to_s
       redirect_to root_path, notice: "You are logged in!"
