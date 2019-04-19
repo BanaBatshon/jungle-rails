50c36e4cdd9ec5f44117537eaff89474ffff0328
Bana <banabatshon@hotmail.com>
2019-04-19 23:34:33 +0000

    removes unnecessary timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190419220712) do
+ActiveRecord::Schema.define(version: 20190419230258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,6 @@ ActiveRecord::Schema.define(version: 20190419220712) do
     t.integer  "user_id"
     t.text     "description"
     t.integer  "rating"
-    t.datetime "ceatedat"
-    t.datetime "updatedat"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
