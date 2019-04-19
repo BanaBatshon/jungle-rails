a4bf4de066d8f9f7c194d3c39290fb1da5b73410
Bana <banabatshon@hotmail.com>
2019-04-19 18:55:30 +0000

    sets the default mail address and defines an email_receipt method that send a msg with the order ID to the user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,11 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def email_receipt(user, order)
+    @user = user
+    @order = order
+    @url  = '/orders/:id'
+    mail(to: order.email, subject: 'Here is your order ID for reference: #{order.id}')
+  end
+
 end
