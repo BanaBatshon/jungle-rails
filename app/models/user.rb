36e1c5ec379c271f61ff1b73c5075ee484b5b90b
Bana <banabatshon@hotmail.com>
2019-04-22 23:03:04 +0000

    update controlle to use user_authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
    has_secure_password
    validates :email, presence: true, uniqueness: true
-  end
+   validates :password, presence: true
+   validates :password_confirmation, presence: true
+
+   def self.authenticate_with_credentials (email, password)
+      user = User.find_by_email(email)
+      if user && user.authenticate(password)
+        return user
+      else 
+        return nil
+      end
+   end
+
+end
