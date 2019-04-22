df3b3fd757c1f35241fd8cc59c5ac7d36f1525db
Bana <banabatshon@hotmail.com>
2019-04-22 00:13:39 +0000

    defines a before filter method to prevent non logged in users from accessing the create review method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_filter :deny_access
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
@@ -13,5 +14,8 @@ class ReviewsController < ApplicationController
   private
   def review_params
     params.require(:review).permit(:description, :rating)
+  end
+  def deny_access
+    raise 'you must be logged in to leave a review' unless @current_user
   end
 end
