31fe4b920020479797fcc7791f7259c458b64a3b
Bana <banabatshon@hotmail.com>
2019-04-22 19:40:58 +0000

    adds the field password to the db

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 20190419230258) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password"
+    t.string   "password_confirmation"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
