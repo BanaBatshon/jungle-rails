df1123ae0df3208c399be0381fe262c4080cb022
Bana <banabatshon@hotmail.com>
2019-04-23 00:11:35 +0000

    saves the registered email as lowercase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    user = User.new(user_params) do |u|
+      u.email = u.email.downcase
+    end
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
