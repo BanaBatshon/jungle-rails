216adff3f51cd09c2e92922c31a806f4973397fc
Bana <banabatshon@hotmail.com>
2019-04-23 00:15:07 +0000

    loggs in the user even if they enter an upper case email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
    has_secure_password
-   validates :email, presence: true, uniqueness: true
+   validates :email, presence: true, uniqueness: true 
    validates :password, presence: true
    validates :password_confirmation, presence: true
 
    def self.authenticate_with_credentials (email, password)
-      user = User.find_by_email(email)
+      user = User.find_by_email(email.strip.downcase)
       if user && user.authenticate(password)
         return user
       else 
